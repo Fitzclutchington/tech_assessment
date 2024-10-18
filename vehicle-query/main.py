a8ed68ff5ca95d3dacbf5157a0fce806b64e2161
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2024-10-18 10:40:04 -0400

    add initial aggregation code

--- vehicle-query/main.py
+++ vehicle-query/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from query import fetch_ev_data, format
 
@@ -7,5 +7,9 @@ app = FastAPI()
 @app.get("vehicles/{model_year}")
 async def get_vehicles(model_year: int):
     ev_data = fetch_ev_data(model_year)
+    
+    if len(ev_data) == 0:
+        raise HTTPException(status_code=404, detail=f"No data found for model year {model_year}")
+    
     stats_by_make: dict[str, dict[str, int | float]] = format(ev_data)
     return stats_by_make
--- vehicle-query/query.py
+++ vehicle-query/query.py
@@ -1,13 +1,23 @@
-import requests
 import typing as t
+
+import pandas as pd
+import requests
 
 BASE_URL = "https://data.wa.gov/resource/f6w7-q2d2.json"
 
 
-def fetch_ev_data(model_year: int) -> dict[str, t.Any]:
+def fetch_ev_data(model_year: int) -> list[dict[str, t.Any]]:
     year_url = f"{BASE_URL}?model_year={model_year}"
     r = requests.get(year_url)
     return r.json()
 
-def format(ev_data: dict[str, t.Any]) -> dict[str, dict[str, int | float]]:
-    pass
+
+def format(ev_data: list[dict[str, t.Any]]) -> list[dict[str, int | float | str]]:
+    ev_df = pd.DataFrame(ev_data)
+    ev_df["electric_range"] = pd.to_numeric(ev_df["electric_range"], errors="coerce")
+    aggregated_data = (
+        ev_df.groupby("make")
+        .agg(total_vehicles=("make", "size"), average_range=("electric_range", "mean"))
+        .reset_index()
+    )
+    return aggregated_data.to_dict(orient="records")

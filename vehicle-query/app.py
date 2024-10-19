a1fe4beae9d33abd0a41a855360440efb09d9159
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2024-10-19 12:04:14 -0400

    update route logic if no files are pulled

--- vehicle-query/app.py
+++ vehicle-query/app.py
@@ -1,30 +1,24 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
+from .schemas import EVDataResponse
 from .query import fetch_ev_data, format
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-class EVDataResponse(BaseModel):
-    make: str
-    total_vehicles: int
-    average_range: float
-
-
 @app.get("/vehicles/{model_year}", status_code=200)
 async def get_vehicles(request: Request, model_year: int):
     ev_data = fetch_ev_data(model_year)
 
-    if len(ev_data) == 0:
-        raise HTTPException(
-            status_code=404, detail=f"No data found for model year {model_year}"
-        )
-
     stats_by_make: list[EVDataResponse] = format(ev_data)
     return templates.TemplateResponse(
         "vehicles.html",
-        {"request": request, "ev_data": stats_by_make, "model_year": model_year, "title": "Vehicles By Year"},
+        {
+            "request": request,
+            "ev_data": stats_by_make,
+            "model_year": model_year,
+            "title": "Vehicles By Year",
+        },
     )
--- vehicle-query/query.py
+++ vehicle-query/query.py
@@ -4,6 +4,12 @@ import pandas as pd
 import requests
 from fastapi import HTTPException
 
+from .schemas import EVDataResponse
+
+# TODO: consider a class per data source
+# Pros - DRY functions for fetching and formatting data
+# considerations - What if number of data sources balloons?
+
 BASE_URL = "https://data.wa.gov/resource/f6w7-q2d2.json"
 
 
@@ -18,7 +24,11 @@ def fetch_ev_data(model_year: int) -> list[dict[str, t.Any]]:
     return r.json()
 
 
-def format(ev_data: list[dict[str, t.Any]]) -> list[dict[str, int | float | str]]:
+def format(ev_data: list[dict[str, t.Any]]) -> EVDataResponse:
+
+    if len(ev_data) == 0:
+        return []
+
     ev_df = pd.DataFrame(ev_data)
     ev_df["electric_range"] = pd.to_numeric(ev_df["electric_range"], errors="coerce")
     aggregated_data = (
@@ -26,4 +36,15 @@ def format(ev_data: list[dict[str, t.Any]]) -> list[dict[str, int | float | str]
         .agg(total_vehicles=("make", "size"), average_range=("electric_range", "mean"))
         .reset_index()
     )
-    return aggregated_data.to_dict(orient="records")
+    # convert to list of EVDataResponse to ensure data integrity
+    # NOTE: not sure if this is the most efficient way to do this.
+    formatted_data = aggregated_data.apply(
+        lambda row: EVDataResponse(
+            make=row["make"],
+            total_vehicles=row["total_vehicles"],
+            average_range=row["average_range"],
+        ),
+        axis=1,
+    ).tolist()
+
+    return formatted_data

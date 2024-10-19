c636c1cc35324862513c74bb054db059cc529e45
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2024-10-19 11:51:09 -0400

    update vehicles route to display data as a table

--- vehicle-query/app.py
+++ vehicle-query/app.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
 from .query import fetch_ev_data, format
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 class EVDataResponse(BaseModel):
@@ -12,12 +14,8 @@ class EVDataResponse(BaseModel):
     average_range: float
 
 
-@app.get(
-    "/vehicles/{model_year}",
-    response_model=list[EVDataResponse], 
-    status_code=200
-)
-async def get_vehicles(model_year: int):
+@app.get("/vehicles/{model_year}", status_code=200)
+async def get_vehicles(request: Request, model_year: int):
     ev_data = fetch_ev_data(model_year)
 
     if len(ev_data) == 0:
@@ -25,5 +23,8 @@ async def get_vehicles(model_year: int):
             status_code=404, detail=f"No data found for model year {model_year}"
         )
 
-    stats_by_make: dict[str, dict[str, int | float]] = format(ev_data)
-    return stats_by_make
+    stats_by_make: list[EVDataResponse] = format(ev_data)
+    return templates.TemplateResponse(
+        "vehicles.html",
+        {"request": request, "ev_data": stats_by_make, "model_year": model_year, "title": "Vehicles By Year"},
+    )

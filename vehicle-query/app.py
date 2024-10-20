5bfb70b8ed3e56cf578b499dc91699576444230e
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2024-10-20 12:40:21 -0400

    add form for submitting year, use prg pattern

--- vehicle-query/app.py
+++ vehicle-query/app.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
-from .schemas import EVDataResponse
 from .query import fetch_ev_data, format
+from .schemas import EVDataResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/vehicles/{model_year}", status_code=200)
-async def get_vehicles(request: Request, model_year: int):
+@app.get("/", response_class=HTMLResponse, status_code=200)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/submit-year")
+async def submit_year(model_year: int = Form(...)):
+
+    return RedirectResponse(url=f"/vehicles?model_year={model_year}", status_code=303)
+
+
+@app.get("/vehicles", response_class=HTMLResponse)
+async def vehicles(request: Request, model_year: int | None = None):
+    # TODO: handle case where year is None, need some refactoring 
+    # consider future query args like model or make
     ev_data = fetch_ev_data(model_year)
 
     stats_by_make: list[EVDataResponse] = format(ev_data)
